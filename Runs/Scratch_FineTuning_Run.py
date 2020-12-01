4774ea01e00c59e6140830b9e9607ea8982ba8a3
daigokanda <kanda@cmu.iit.ac.jp>
2020-12-01 14:46:39 +0900

    uncomment

--- Runs/Scratch_FineTuning_Run.py
+++ Runs/Scratch_FineTuning_Run.py
@@ -1,3 +1,4 @@
+import finetuning
 import scratch
 
 for i in range(10):
@@ -7,9 +8,9 @@ for i in range(10):
                                     "64", "--epochs", "100", "--trained_model", "model/my_model.hdf5"])
                  )
 
-# for i in range(10):
-#     parser = finetuning.getParser()
-#     finetuning.main(parser.parse_args(["0", "/mnt/data/DataSet/GazeCapture_pre/00010", "./", "--image_size", "224",
-#                                        "--batch_size",
-#                                        "64", "--epochs", "100", "--trained_model", "model/my_model.hdf5"])
-#                     )
+for i in range(10):
+    parser = finetuning.getParser()
+    finetuning.main(parser.parse_args(["0", "/mnt/data/DataSet/GazeCapture_pre/00010", "./", "--image_size", "224",
+                                       "--batch_size",
+                                       "64", "--epochs", "100", "--trained_model", "model/my_model.hdf5"])
+                    )
