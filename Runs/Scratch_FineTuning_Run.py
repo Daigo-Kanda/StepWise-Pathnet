768ccadeef2e1fa92a0f2c765cfaeee205a908f5
daigokanda <kanda@cmu.iit.ac.jp>
2020-12-01 14:36:52 +0900

    Run file for Scratch and Fine tuning

---
+++ Runs/Scratch_FineTuning_Run.py
@@ -0,0 +1,15 @@
+import scratch
+
+for i in range(10):
+    parser = scratch.getParser()
+    scratch.main(parser.parse_args(["0", "/mnt/data/DataSet/GazeCapture_pre/00010", "./", "--image_size", "224",
+                                    "--batch_size",
+                                    "64", "--epochs", "100", "--trained_model", "model/my_model.hdf5"])
+                 )
+
+# for i in range(10):
+#     parser = finetuning.getParser()
+#     finetuning.main(parser.parse_args(["0", "/mnt/data/DataSet/GazeCapture_pre/00010", "./", "--image_size", "224",
+#                                        "--batch_size",
+#                                        "64", "--epochs", "100", "--trained_model", "model/my_model.hdf5"])
+#                     )
